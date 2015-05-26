144a514fd330c177abc4db4fa1ab8a14d6176b74
tanhaoqin <tanhq646@gmail.com>
2015-05-26 11:06:02 +0800

    Added dollar sign...

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -38,8 +38,8 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
 		$scope.posts.push({
 			title: $scope.title, 
 			link: $scope.link,
-			upvotes:0,
-			comments:[
+			upvotes: 0,
+			comments: [
 				{author: 'Joe', body:'Cool post!', upvotes:0},
 				{author: 'Bob', body:'Great idea but everything is wrong!', upvotes:0},
 			]});
@@ -51,14 +51,15 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
 	};
 }]);
 
-app.controller('PostsCtrl', ['$scope', 
+app.controller('PostsCtrl', [
+	'$scope', 
 	'$stateParams', 
 	'posts', 
 	function($scope, $stateParams, posts){
 		$scope.post = posts.posts[$stateParams.id];
 		$scope.addComment = function(){
 			if($scope.body === '') {return;}
-			$scope.body.comments.push({
+			$scope.post.comments.push({
 				body: $scope.body,
 				author: 'user',
 				upvotes: 0
