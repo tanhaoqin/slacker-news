80828a1853655c1832a60619224ed4bf900ca0ea
tanhaoqin <tanhq646@gmail.com>
2015-05-26 12:20:07 +0800

    Added functionality to add posts

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -31,6 +31,11 @@ app.factory('posts', ['$http', function($http){
 			angular.copy(data, o.posts);
 		});
 	};
+	o.create = function(post){
+		return $http.post('/posts',post).success(function(data){
+			o.posts.push(data);
+		});
+	};
 	return o;
 }]);
 
@@ -45,14 +50,18 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
 	// ];
 	$scope.addPost = function(){
 		if(!$scope.title || $scope.title === ''){ return;}
-		$scope.posts.push({
+		posts.create({
 			title: $scope.title, 
 			link: $scope.link,
-			upvotes: 0,
-			comments: [
-				{author: 'Joe', body:'Cool post!', upvotes:0},
-				{author: 'Bob', body:'Great idea but everything is wrong!', upvotes:0},
-			]});
+		});
+		// $scope.posts.push({
+		// 	title: $scope.title, 
+		// 	link: $scope.link,
+		// 	upvotes: 0,
+		// 	comments: [
+		// 		{author: 'Joe', body:'Cool post!', upvotes:0},
+		// 		{author: 'Bob', body:'Great idea but everything is wrong!', upvotes:0},
+		// 	]});
 		$scope.title = '';
 		$scope.link = '';
 	};
